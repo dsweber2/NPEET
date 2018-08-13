5e15a2b906f92b824c918829b8a8fb372d4635b4
dsweber2 <david.weber2@gmail.com>
2018-08-13 15:05:52 -0400

    parallel version of kd-trees

--- entropy_estimators.py
+++ entropy_estimators.py
@@ -9,10 +9,116 @@ from math import log
 import numpy.random as nr
 import numpy as np
 import random
+from numpy import linalg as la
+try:
+    import multiprocessing as processing
+except:
+    import processing
+
+
+
+
+def __remote_process_query(rank, qin, qout, tree, K, leafsize):
+    while 1:
+        # read input queue (block until data arrives)
+        nc, data = qin.get()
+        # search for the distance to the k nearest points; the blank is their locations
+        knn,_ = tree.query(data, K, p=float('inf'))
+        # write to output queue
+        qout.put((nc,knn))
+
+def __remote_process_ball(rank, qin, qout, tree, leafsize):
+    while 1:
+        # read input queue (block until data arrives)
+        nc, data, eps = qin.get()
+        assert len(eps) == data.shape[0]
+        knn = []
+        #listOfPoints = tree.query_ball_point(data, eps, p=float('inf'))
+        for i in range(len(eps)):
+            dist = eps[i]-1e-15
+            knn += [len(tree.query_ball_point(data[i,:], dist, p=float('inf')))+1]
+        # write to output queue
+        qout.put((nc,knn))
+
+def knn_search_parallel(data, K, qin=None, qout=None, tree=None, t0=None, eps=None, leafsize=None, copy_data=False):
+    """ find the K nearest neighbours for data points in data,
+        using an O(n log n) kd-tree, exploiting all logical
+        processors on the computer. if eps <= 0, it returns the distance to the kth point. On the other hand, if eps > 0 """
+    # print("starting the parallel search")
+    if eps is not None:
+        assert data.shape[0]==len(eps)
+    # build kdtree
+    if copy_data:
+        dataCopy = data.copy()
+        # print('copied data')
+    else:
+        dataCopy = data
+    if tree is None and leafsize is None:
+        tree = ss.cKDTree(dataCopy)
+    elif tree is None:
+        tree = ss.cKDTree(dataCopy, leafsize=leafsize)
+    if t0 is not None:
+        print('time to tree formation: %f' %(clock()-t0))
+    ndata = data.shape[0]
+    nproc = 20
+    # print('made the tree')
+    # compute chunk size
+    chunk_size = int(data.shape[0] / (4*nproc))
+    chunk_size = 100 if chunk_size < 100 else chunk_size
+    if qin==None or qout==None:
+        # set up a pool of processes
+        qin = processing.Queue(maxsize=int(ndata/chunk_size))
+        qout = processing.Queue(maxsize=int(ndata/chunk_size))
+    if eps is None:
+        pool = [processing.Process(target=__remote_process_query,
+                                   args=(rank, qin, qout, tree, K, leafsize))
+                for rank in range(nproc)]
+    else:
+        pool = [processing.Process(target=__remote_process_ball,
+                                   args=(rank, qin, qout, tree, leafsize))
+                for rank in range(nproc)]
+    for p in pool: p.start()
+    # put data chunks in input queue
+    cur, nc = 0, 0
+    while 1:
+        _data = data[cur:cur+chunk_size, :]
+        if _data.shape[0] == 0: break
+        if eps is None:
+            qin.put((nc,_data))
+        else:
+            _eps = eps[cur:cur+chunk_size]
+            qin.put((nc,_data,_eps))
+        cur += chunk_size
+        nc += 1
+    # read output queue
+    knn = []
+    while len(knn) < nc:
+        knn += [qout.get()]
+        # avoid race condition
+        _knn = [n for i,n in sorted(knn)]
+    knn = []
+    for tmp in _knn:
+        knn += [tmp]
+    # terminate workers
+    for p in pool: p.terminate()
+
+    if eps is None:
+        output = np.zeros((sum([ x.shape[0] for x in knn]),knn[0].shape[1]))
+    else:
+        output = np.zeros(sum([ len(x) for x in knn]))
+    outputi = 0
+    for x in knn:
+        if eps is None:
+            nextVal = x.shape[0]
+        else:
+            nextVal = len(x)
+        output[outputi:(outputi+nextVal)] = x
+        outputi += nextVal
+    return output
 
 # CONTINUOUS ESTIMATORS
 
-def entropy(x, k=3, base=2):
+def entropy(x, k=3, base=2.0,printing=False, qin=None, qout=None):
     """ The classic K-L k-nearest neighbor continuous entropy estimator
         x should be a list of vectors, e.g. x = [[1.3], [3.7], [5.1], [2.4]]
         if x is a one-dimensional scalar and we have four samples
@@ -21,11 +127,15 @@ def entropy(x, k=3, base=2):
     d = len(x[0])
     N = len(x)
     intens = 1e-10  # small noise to break degeneracy, see doc.
-    x = [list(p + intens * nr.rand(len(x[0]))) for p in x]
-    tree = ss.cKDTree(x)
-    nn = [tree.query(point, k + 1, p=float('inf'))[0][k] for point in x]
+    x = x + intens*nr.rand(x.shape[0],x.shape[1])
+    nn = knn_search_parallel(x,k,qin=qin, qout=qout)[:,-1]
+    #tree = ss.ccKDTree(x)
+    # [tree.query(point, k + 1, p=float('inf'))[0][k] for point in x]
     const = digamma(N) - digamma(k) + d * log(2)
-    return (const + d * np.mean(map(log, nn))) / log(base)
+    h = (const + d * np.mean(np.log(nn))) / log(base)
+    if printing:
+        print(h)
+    return h
 
 def centropy(x, y, k=3, base=2):
   """ The classic K-L k-nearest neighbor continuous entropy estimator for the
@@ -50,25 +160,28 @@ def corex(xs, ys, k=3, base=2):
   cxis = [mi(column(xs, i), ys, k, base) for i in range(0, len(xs[0]))]
   return np.sum(cxis) - mi(xs, ys, k, base)
 
-def mi(x, y, k=3, base=2):
+def mi(xp, yp, k=3, base=2,normalize=True,qin=None, qout=None):
     """ Mutual information of x and y
-        x, y should be a list of vectors, e.g. x = [[1.3], [3.7], [5.1], [2.4]]
-        if x is a one-dimensional scalar and we have four samples
+        x, y should eiter be a list of vectors, e.g. x = [[1.3], [3.7], [5.1], [2.4]] or a matrix with (#examples x dimension). Their types should match
     """
-    assert len(x) == len(y), "Lists should have same length"
-    assert k <= len(x) - 1, "Set k smaller than num. samples - 1"
-    intens = 1e-10  # small noise to break degeneracy, see doc.
-    x = [list(p + intens * nr.rand(len(x[0]))) for p in x]
-    y = [list(p + intens * nr.rand(len(y[0]))) for p in y]
-    points = zip2(x, y)
+    assert xp.shape[0] == yp.shape[0], "Lists should have same length"
+    assert type(xp[0])==type(yp[0]), "Should be the same kind of list"
+    assert k <= len(xp) - 1, "Set k smaller than num. samples - 1. Way smaller."
+    x = (xp-np.mean(xp,axis=0))/np.std(xp, axis=0)
+    y = (yp-np.mean(yp,axis=0))/np.std(yp, axis=0)
+    intens = 1e-10
+    points = np.append(x,y,axis=1) + intens*nr.rand(x.shape[0],x.shape[1]+y.shape[1])
     # Find nearest neighbors in joint space, p=inf means max-norm
-    tree = ss.cKDTree(points)
-    dvec = [tree.query(point, k + 1, p=float('inf'))[0][k] for point in points]
-    a, b, c, d = avgdigamma(x, dvec), avgdigamma(y, dvec), digamma(k), digamma(len(x))
-    return (-a - b + c + d) / log(base)
+    dvec = knn_search_parallel(points, k+1, qin=qin, qout=qout)
+    dvec = dvec[:,-1]
+    #tree = ss.ccKDTree(points)
+    #dvec = [tree.query(point, k + 1, p=float('inf'))[0][k] for point in points]
+    a, b, c, d = avgdigamma(x, dvec,qin=qin, qout=qout), avgdigamma(y, dvec, qin=qin, qout=qout), digamma(k), digamma(points.shape[0])
+    mmi = (-a - b + c + d) / log(base)
+    return mmi
 
 
-def cmi(x, y, z, k=3, base=2):
+def cmi(x, y, z, k=3, base=2, qin=None, qout=None):
     """ Mutual information of x and y, conditioned on z
         x, y, z should be a list of vectors, e.g. x = [[1.3], [3.7], [5.1], [2.4]]
         if x is a one-dimensional scalar and we have four samples
@@ -81,13 +194,14 @@ def cmi(x, y, z, k=3, base=2):
     z = [list(p + intens * nr.rand(len(z[0]))) for p in z]
     points = zip2(x, y, z)
     # Find nearest neighbors in joint space, p=inf means max-norm
-    tree = ss.cKDTree(points)
-    dvec = [tree.query(point, k + 1, p=float('inf'))[0][k] for point in points]
-    a, b, c, d = avgdigamma(zip2(x, z), dvec), avgdigamma(zip2(y, z), dvec), avgdigamma(z, dvec), digamma(k)
+    dvec = knn_search_parallel(points,k,qin=qin, qout=qout)[:,-1]
+    #tree = ss.ccKDTree(points)
+    #[tree.query(point, k + 1, p=float('inf'))[0][k] for point in points]
+    a, b, c, d = avgdigamma(zip2(x, z), dvec,qin=qin, qout=qout), avgdigamma(zip2(y, z), dvec), avgdigamma(z, dvec), digamma(k)
     return (-a - b + c + d) / log(base)
 
 
-def kldiv(x, xp, k=3, base=2):
+def kldiv(x, xp, k=3, base=2, qin=None, qout=None):
     """ KL Divergence between p and q for x~p(x), xp~q(x)
         x, xp should be a list of vectors, e.g. x = [[1.3], [3.7], [5.1], [2.4]]
         if x is a one-dimensional scalar and we have four samples
@@ -99,10 +213,12 @@ def kldiv(x, xp, k=3, base=2):
     n = len(x)
     m = len(xp)
     const = log(m) - log(n - 1)
-    tree = ss.cKDTree(x)
-    treep = ss.cKDTree(xp)
-    nn = [tree.query(point, k + 1, p=float('inf'))[0][k] for point in x]
-    nnp = [treep.query(point, k, p=float('inf'))[0][k - 1] for point in x]
+    tree = ss.ccKDTree(x)
+    treep = ss.ccKDTree(xp)
+    nn = knn_search_parallel(x, k, tree=tree, qin=qin, qout=qout)
+    #[tree.query(point, k + 1, p=float('inf'))[0][k] for point in x]
+    nnp = knn_search_parallel(x, k-1, tree=treep, qin=qin, qout=qout)
+    #nnp = [treep.query(point, k, p=float('inf'))[0][k - 1] for point in x]
     return (const + d * np.mean(map(log, nnp)) - d * np.mean(map(log, nn))) / log(base)
 
 
@@ -167,14 +283,15 @@ def elog(x):
         return 0
     else:
         return x * log(x)
-
-
+tmp = [x for x in range(20)]
 # MIXED ESTIMATORS
-def micd(x, y, k=3, base=2, warning=True):
+def micd(x, y, k=3, base=2, warning=True, qin=None,qout=None):
     """ If x is continuous and y is discrete, compute mutual information
     """
-    overallentropy = entropy(x, k, base)
+    overallentropy = entropy(x, k, base, qin=qin,qout=qout)
 
+    if type(y)==np.ndarray:
+        y = y.tolist()
     n = len(y)
     word_dict = dict()
     for i in range(len(y)):
@@ -186,16 +303,17 @@ def micd(x, y, k=3, base=2, warning=True):
 
     mi = overallentropy
     for yval in yvals:
-        xgiveny = [x[i] for i in range(n) if y[i] == yval]
+        xgiveny = x[[k==1 for k in y],:]
         if k <= len(xgiveny) - 1:
-            mi -= word_dict[yval] * entropy(xgiveny, k, base)
+            mi -= word_dict[yval] * entropy(xgiveny, k, base,qin=qin,qout=qout)
         else:
             if warning:
                 print("Warning, after conditioning, on y=", yval, " insufficient data. Assuming maximal entropy in this case.")
             mi -= word_dict[yval] * overallentropy
+    print(np.abs(mi))
     return np.abs(mi)  # units already applied
 
-def midc(x, y, k=3, base=2, warning=True):
+def midc(x, y, k=3, base=2, warning=True, qin=None,qout=None):
   return micd(y, x, k, base, warning)
 
 def centropydc(x, y, k=3, base=2, warning=True):
@@ -250,18 +368,22 @@ def shuffle_test(measure, x, y, z=False, ns=200, ci=0.95, **kwargs):
 
 # INTERNAL FUNCTIONS
 
-def avgdigamma(points, dvec):
+def avgdigamma(points, dvec, qin=None, qout=None):
     # This part finds number of neighbors in some radius in the marginal space
     # returns expectation value of <psi(nx)>
-    N = len(points)
-    tree = ss.cKDTree(points)
+    N = points.shape[0]
     avg = 0.
-    for i in range(N):
-        dist = dvec[i]
-        # subtlety, we don't include the boundary point,
-        # but we are implicitly adding 1 to kraskov def bc center point is included
-        num_points = len(tree.query_ball_point(points[i], dist - 1e-15, p=float('inf')))
-        avg += digamma(num_points) / N
+    num_points = knn_search_parallel(points, 3, eps=dvec-1e-15, qin=qin, qout=qout)
+    avg = sum([digamma(x)/N for x in num_points])
+    #tree = ss.ccKDTree(points)
+
+    #for i in range(N):
+    #dist = dvec[i]
+    # subtlety, we don't include the boundary point,
+    # but we are implicitly adding 1 to kraskov def bc center point is included
+
+    #num_points = len(tree.query_ball_point(points[i,:], dist - 1e-15, p=float('inf')))
+    #avg += digamma(num_points) / N
     return avg
 
 
@@ -279,5 +401,26 @@ def discretize(xs):
     # discretize(xs) takes a list of vectors and makes it a list of tuples or scalars
     return [discretize_one(x) for x in xs]
 
-if __name__ == "__main__":
-    print("NPEET: Non-parametric entropy estimation toolbox. See readme.pdf for details on usage.")
+#if __name__ == "__main__":
+#    print("NPEET: Non-parametric entropy estimation toolbox. See readme.pdf for details on usage.")
+
+from time import clock
+from numpy import random as rand
+def test(t0):
+    return knn_search_parallel(data, K,t0=t0)
+
+if __name__ == '__main__':
+    dY, dX = (3072,3072)
+    ndata = 6000
+    A = np.block([[10*nr.randn(dX,dX), nr.randn(dX,dY)], [nr.randn(dY,dX), 10*nr.randn(dY,dY)]])
+    A = A.T@A
+    AX = A[0:dX,0:dX]
+    AY = A[dX:,dX:]
+    data = 10*nr.multivariate_normal(np.zeros(dX+dY), A, ndata)
+    eps = 3*nr.rand(ndata)
+    ss.cKDTree(data.copy())
+    #print('avgdigamma with random eps: %f' % test3)
+    print('Testing mutual information calculation:')
+    mi(data[:,0:dX],data[:,dX:])
+    print('Theoretical Value')
+    print((la.slogdet(AX)[1]+la.slogdet(AY)[1]-la.slogdet(A)[1])/2)
